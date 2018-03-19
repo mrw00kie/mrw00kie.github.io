1c4e0e6dbcbdbe4429a15df13090b3b44140ccbf
mrw00kie <karlfried3@yahoo.de>
2018-03-19 10:50:41 +0100

    ..

--- js/handel/context.js
+++ js/handel/context.js
@@ -38,10 +38,10 @@ function context_mousePressed()
 				var _y = i * 60 + 10;
 				//that.rect(_x, _y,  236, 50);
 				console.log(mouseY);
-				
+
 				if(mouseY > _y && mouseY < _y + 50 - 720){
 					console.log(tech_tree[i].getAttribute('name'));
-					console.log(tech_tree[i].getElementsByTagName('cost')[0].innerHTML);
+					//console.log(tech_tree[i].getElementsByTagName('cost')[0].innerHTML);
 				}
 			}
 		}
