1dd0a3bb261e8a6c7f7b016b09336ea4b176be98
mrw00kie <karlfried3@yahoo.de>
2018-03-22 02:06:30 +0100

    ..

---
+++ empty-example/sketch.js
@@ -0,0 +1,47 @@
+// Research Variables
+var list_research_id;
+var list_research_info;
+var list_completed_research;
+var list_research_cost;
+
+var game_research_object;
+
+var xml;
+var xml_technologie_tree;
+
+
+function preload() {
+	xml = loadXML('xml/technologie.xml');
+}
+
+
+function setup() {
+
+	xml_technologie_tree = xml.getChildren('tech');
+
+	can = createCanvas(1280, 720);
+	can.parent('game');
+
+	// Research Variables
+	list_research_id = new Array();
+	list_research_info = new Array();
+	list_completed_research = new Array();
+	list_research_cost = new Array();
+
+	for(var i = 0; i < 6; i++) {
+		list_research_id[i] = "EMPTY";
+		list_research_info[i] = "";
+		list_research_cost[i] = 0;
+	}
+
+	// Game Variables
+	game_research_object = new object_research();
+
+	getAvalibleResearch();
+
+	setup_research_selection();
+}
+
+function draw()  {
+	can.background(51);
+}
