4a37aeeb115434e535f2d1a5fffe63ef25530051
mrw00kie <karlfried3@yahoo.de>
2018-03-23 23:05:43 +0100

    fixing occuring problems

--- empty-example/js/hud/navigation_bar.js
+++ empty-example/js/hud/navigation_bar.js
@@ -6,5 +6,5 @@ function update_navigation_bar() {
 	document.getElementById("navigation_research_value").innerHTML = String(game_research.researchRate);
 	document.getElementById("navigation_production_value").innerHTML = String(game_productuion.productionRate);
 	document.getElementById("navigation_power_value").innerHTML = String(recource_power);
-	document.getElementById("navigation_materials_vale").innerHTML = String(recource_materials);
+	document.getElementById("navigation_materials_value").innerHTML = String(recource_materials);
 }
