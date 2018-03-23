87f17c6649eba222c0373486ea5663ceb0125bc6
mrw00kie <karlfried3@yahoo.de>
2018-03-24 00:10:56 +0100

    window selection

--- empty-example/js/hud/navigation_bar.js
+++ empty-example/js/hud/navigation_bar.js
@@ -1,5 +1,5 @@
 
-
+var dropdown_navigation_selection;
 
 function update_navigation_bar() {
 	document.getElementById("navigation_money_value").innerHTML = String(recource_money);
@@ -7,4 +7,22 @@ function update_navigation_bar() {
 	document.getElementById("navigation_production_value").innerHTML = String(game_productuion.productionRate);
 	document.getElementById("navigation_power_value").innerHTML = String(recource_power);
 	document.getElementById("navigation_materials_value").innerHTML = String(recource_materials);
+
+
+	dropdown_navigation_selection = createElement();
+	dropdown_navigation_selection.parent(navigation_bar);
+	dropdown_navigation_selection.style('position', 'relative');
+	dropdown_navigation_selection.style('left', '400px');
+	dropdown_navigation_selection.style('top', '2px');
+	dropdown_navigation_selection.id('dropdown_navigation_selection');
+	dropdown_navigation_selection.option('dropdown_navigation_research', 'Research');
+	dropdown_navigation_selection.option('dropdown_navigation_buildings', 'Buildings');
+	dropdown_navigation_selection.option('dropdown_navigation_units', 'Units');
+	dropdown_navigation_selection.option('dropdown_navigation_materials', 'Materials');
+
+	var dropd = document.getElementById("dropdown_navigation_selection");
+	for(var i = 0; i < 4; i++) {
+		dropd.options[i].id = "dropdown_navigation_option_"+String(i);
+	}
+
 }
