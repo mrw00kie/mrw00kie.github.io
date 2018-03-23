3e92908dcc64e4489d7dd9ae6e747a4857f4b043
mrw00kie <karlfried3@yahoo.de>
2018-03-23 23:00:08 +0100

    add icons to nav-bar

--- empty-example/js/hud/navigation_bar.js
+++ empty-example/js/hud/navigation_bar.js
@@ -2,9 +2,9 @@
 
 
 function update_navigation_bar() {
-	document.getElementById("navigation_money").innerHTML = "Money: "+String(recource_money)+"$";
-	document.getElementById("navigation_research").innerHTML = "Research: "+String(game_research.researchRate);
-	document.getElementById("navigation_production").innerHTML = "Production: "+String(game_productuion.productionRate);
-	document.getElementById("navigation_power").innerHTML = "Power: "+String(recource_power)+"kW";
-	document.getElementById("navigation_materials").innerHTML = "Mterials: "+String(recource_materials);
+	document.getElementById("navigation_money_value").innerHTML = String(recource_money);
+	document.getElementById("navigation_research_value").innerHTML = String(game_research.researchRate);
+	document.getElementById("navigation_production_value").innerHTML = String(game_productuion.productionRate);
+	document.getElementById("navigation_power_value").innerHTML = String(recource_power);
+	document.getElementById("navigation_materials_vale").innerHTML = String(recource_materials);
 }
